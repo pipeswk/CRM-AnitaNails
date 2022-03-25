415446dd723ff0ec2ce48a23f570144e4c815070
pipeswk <afco1995@gmail.com>
2022-03-25 17:47:30 -0500

    Se agrega Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './Layout/Layout'
+import Inicio from './Pages/Inicio'
+import NuevoCliente from './Pages/NuevoCliente'
+import EditarCliente from './Pages/EditarCliente'
+
 
 function App() {
 
   return (
-    <div>
-      Desde App
-    </div>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/clientes' element={<Layout />}>
+            <Route index element={<Inicio />} />
+            <Route path='nuevo' element={<NuevoCliente />} />
+            <Route path='editar-cliente/:id' element={<EditarCliente />} />
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
   )
 }
 
