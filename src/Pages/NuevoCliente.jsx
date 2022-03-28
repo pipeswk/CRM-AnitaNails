9a00c0b0432f029814a825c0ed6fc9ab8a6a38f6
pipeswk <afco1995@gmail.com>
2022-03-28 17:28:32 -0500

    Componente NuevoCliente v1

--- src/Pages/NuevoCliente.jsx
+++ src/Pages/NuevoCliente.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Formulario from '../Components/Formulario'
 
 const NuevoCliente = () => {
   return (
-    <div>Desde NuevoCliente</div>
+    <>
+      <h1 className='font-black text-4xl text-pink-900'>Nuevo Cliente</h1>
+      <p className='font-semibold text-lg text mt-3'>Llena los siguientes campos para registrar un cliente <span className='text-pink-600 font-bold text-2xl'>Nuevo.</span></p>
+      
+      <Formulario />
+    </>
   )
 }
 
