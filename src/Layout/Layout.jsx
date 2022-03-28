4c869eeb287765bb03b8b7fbbba4a4a034c54c3b
pipeswk <afco1995@gmail.com>
2022-03-28 17:27:55 -0500

    Nuevas clases (Estilos)

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,28 +1,40 @@
-import { Outlet, NavLink, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 const Layout = () => {
+
+  const location = useLocation();
+  const urlActual = location.pathname;
+
   return (
     <div className='md:flex md:min-h-screen'>
 
-      <div className='md:w-1/4 bg-pink-300 px-5 py-10 rounded-xl m-1 shadow-xl'>
-        <h2 className='text-4xl font-black text-center'>CRM - Clientes</h2>
+      <div className='md:w-1/4 bg-pink-300 px-5 py-10 md:rounded-xl md:m-1 shadow-xl'>
+        <h2 className='text-4xl font-black text-center'>CRM - Anita Nails</h2>
         <nav className='mt-10'>
           <Link
             to="/clientes"
-            className='text-2xl font-semibold block mt-2 hover:text-pink-700'
+            className={`${urlActual === '/clientes' ? (
+              'text-2xl font-semibold block mt-2 text-pink-700'
+            ) : (
+              'text-2xl font-semibold block mt-2 hover:text-pink-700'
+            )}`}
           >
             Clientes
           </Link>
           <Link
             to="/clientes/nuevo"
-            className='text-2xl font-semibold block mt-2 hover:text-pink-700'
+            className={`${urlActual === '/clientes/nuevo' ? (
+              'text-2xl font-semibold block mt-2 text-pink-700'
+            ) : (
+              'text-2xl font-semibold block mt-2 hover:text-pink-700'
+            )}`}
           >
             Nuevo Cliente
           </Link>
         </nav>
       </div>
 
-      <div className='md:w-3/4'>
+      <div className='md:w-3/4 p-10 md:h-screen overflow-y-scroll'>
         <Outlet />
       </div>
 
