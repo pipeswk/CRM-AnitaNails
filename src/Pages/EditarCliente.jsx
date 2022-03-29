0000c5a923470b00305e1098fefbbd781aec77f8
pipeswk <afco1995@gmail.com>
2022-03-29 17:18:35 -0500

    Pagina Editar Cliente v1

--- src/Pages/EditarCliente.jsx
+++ src/Pages/EditarCliente.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import Formulario from '../Components/Formulario'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import Spinner from '../Components/Spinner'
 
 const EditarCliente = () => {
+
+    const [infoCliente, setInfoCliente] = useState({})
+    const [cargando, setCargando] = useState(false)
+
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        setCargando(true);
+        const obtenerClienteAPI = async () => {
+            try {
+                const url = `http://localhost:4000/clientes/${id}`;
+                const respuesta = await fetch(url);
+                const resultado = await respuesta.json();
+                setInfoCliente(resultado);
+            } catch (error) {
+                console.log(error);
+            }
+            setCargando(false);
+        }
+        obtenerClienteAPI();
+    }, [])
+    
+
   return (
-    <div>Desde EditarCliente</div>
+    <>
+      <h1 className='font-black text-4xl text-pink-900'>Editar Cliente</h1>
+      <p className='font-semibold text-lg text mt-3'>Llena los siguientes campos para <span className='text-pink-600 font-bold text-2xl'>EDITAR</span> este cliente </p>
+
+      {cargando ? (
+        <Spinner />
+      ) : (
+        <Formulario
+          id={id}
+          infoCliente={infoCliente}
+        />
+      )}
+    </>
   )
 }
 
