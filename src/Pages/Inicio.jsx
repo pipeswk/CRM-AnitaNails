46336234447d5a0869539636b5995f4722680183
pipeswk <afco1995@gmail.com>
2022-03-29 17:19:38 -0500

    Se agrega: Spinner y metodo "DELETE"

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import Cliente from '../Components/Cliente';
+import Spinner from '../Components/Spinner';
 
 const Inicio = () => {
 
   const [clientes, setClientes] = useState([]);
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
+    setCargando(true);
     const obtenerClientesAPI = async () => {
       try {
         const url = 'http://localhost:4000/clientes';
@@ -15,9 +18,29 @@ const Inicio = () => {
       } catch (error) {
         console.log(error);
       }
+      setCargando(false);
     }
     obtenerClientesAPI();
   }, [])
+
+  const handleDelete = async (id) => {
+    const confirmar = confirm('¿Deseas eliminar a este cliente?')
+
+    if(confirmar) {
+      console.log(`Eliminando cliente id: ${id}`)
+      try {
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url, {
+            method: 'DELETE'
+        })
+        await respuesta.json();
+        const recargarClientes = clientes.filter( cliente => cliente.id !== id )
+        setClientes(recargarClientes)
+      } catch (error) {
+          console.log(error)
+      }
+      }
+  }
   
   console.log(clientes);
   return (
@@ -25,26 +48,31 @@ const Inicio = () => {
       <h1 className='font-black text-4xl text-pink-900 text-center'>Clientes de Anita Nails</h1>
       <p className='font-semibold text-lg text mt-3 text-center'>Aqui podras ver, editar, eliminar y gestionar tu base de <span className='text-pink-600 font-bold text-2xl'>Clientes.</span></p>
 
-      <table className='w-full mt-5 table-auto shadow-xl bg-white rounded-xl'>
-        <thead className='bg-pink-200 rounded-xl'>
-          <tr>
-            <th className='p-2'>Nombre</th>
-            <th className='p-2'>Contacto</th>
-            <th className='p-2'>Cumpleaños</th>
-            <th className='p-2'>Acciones</th>
-          </tr>
-        </thead>
+      {cargando ? (
+        <Spinner />
+      ) : (
+        <table className='w-full mt-5 table-auto shadow-xl bg-white rounded-xl'>
+          <thead className='bg-pink-200 rounded-xl'>  
+            <tr>
+              <th className='p-2'>Nombre</th>
+              <th className='p-2'>Contacto</th>
+              <th className='p-2'>Cumpleaños</th>
+              <th className='p-2'>Acciones</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {clientes.map( cliente => (
-            <Cliente 
-              key={cliente.id}
-              cliente={cliente}
-            />
-          ) )}
-        </tbody>
+          <tbody>
+            {clientes.map( cliente => (
+              <Cliente 
+                key={cliente.id}
+                cliente={cliente}
+                handleDelete={handleDelete}
+              />
+            ) )}
+          </tbody>
 
-      </table>
+        </table>
+      )}
 
     </>
   )
