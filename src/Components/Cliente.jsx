fb3d878247ac835d41f7846e2c2c594cfd9825cd
pipeswk <afco1995@gmail.com>
2022-03-29 11:16:30 -0500

    Se agrega useNavigate al componente

--- src/Components/Cliente.jsx
+++ src/Components/Cliente.jsx
@@ -4,27 +4,27 @@ const Cliente = ( { cliente } ) => {
 
     const { nombres, apellidos, email, telefono, nacimiento, id } = cliente;
 
-    const clickEdiar = () => {
-        console.log('click en editar')
-    }
-
-    const clickVer = () => {
-        console.log('click en ver')
-    }
+    const navigate = useNavigate();
 
   return (
     <tr className='cursor-pointer hover:bg-slate-200'>
-        <td className='p-3' onClick={clickVer}>{`${nombres} ${apellidos}`}</td>
-        <td className='p-3' onClick={clickVer}>
+        <td className='p-3' onClick={() => navigate(`/clientes/${id}`)}>{`${nombres} ${apellidos}`}</td>
+        <td className='p-3' onClick={() => navigate(`/clientes/${id}`)}>
             <p><span className='text-gray-800 font-bold'>Email: </span>{email}</p>
             <p><span className='text-gray-800 font-bold'>Telefono: </span>{telefono}</p>
         </td>
-        <td className='p-3' onClick={clickVer}>{nacimiento}</td>
+        <td className='p-3' onClick={() => navigate(`/clientes/${id}`)}>{nacimiento}</td>
         <td className='p-3'>
-            <button type='button' className='bg-green-600 text-center p-0 text-white font-semibold mx-2 my-1 rounded-md block w-full' onClick={clickEdiar}>
+            <button
+                type='button'
+                className='bg-green-600 text-center p-0 text-white font-semibold mx-2 my-1 rounded-md block w-full'
+                // onClick={}
+            >
                 Editar
             </button>
-            <button type='button' className='bg-red-600 text-center p-0 text-white font-semibold ml-2 my-1 rounded-md block w-full'>
+            <button
+                type='button'
+                className='bg-red-600 text-center p-0 text-white font-semibold ml-2 my-1 rounded-md block w-full'>
                 Eliminar
             </button>
         </td>
